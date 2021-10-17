f89760c6c6613b552fa6465b2b61aad542ab104e
Md Abu Sufian <63811452+mdabusufian@users.noreply.github.com>
2021-10-17 13:37:05 +0600

    Add files via upload

---
+++ Conversion site/history.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>
+    
+  </title>
+</head>
+
+<body>
+<h1>Page 3 [History]</h1>
+<h3>Conversion Site</h3>
+
+1.<a href="home.php">Home</a> 2.<a href="crate.php">Conversion Rate</a> 3.<a href="history.php">History</a><br>
+
+
+
+
+</body>
+
+</html> 
---
+++ Conversion site/home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>
+    
+  </title>
+</head>
+
+<body>
+<h1>Page 1 [Home]</h1>
+<h3>Conversion Site</h3>
+
+1.<a href="home.php">Home</a> 2.<a href="crate.php">Conversion Rate</a> 3.<a href="history.php">History</a><br>
+
+
+  <h3>Converter:</h3>
+
+    Select Category: <select name="cat" id="cat" >
+      <option value="sv">Select A Value</option>
+  </select>
+
+ <br><br>
+
+ Value: <input type="text" name="val" value="">
+
+  <input type="submit" name="Submit">
+
+  <br><br><br>
+
+ Result: <input type="text" name="res" value="">
+
+  </form>
+  <br>
+
+</body>
+
+</html> 
