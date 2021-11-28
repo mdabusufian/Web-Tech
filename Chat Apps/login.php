3674dc6dc26bd2cc1d5871205ef98ab61fb57621
Md Abu Sufian <63811452+mdabusufian@users.noreply.github.com>
2021-11-28 20:34:21 +0600

    Add files via upload

---
+++ Chat Apps/login.php
@@ -0,0 +1,34 @@
+<?php 
+  session_start();
+  if(isset($_SESSION['unique_id'])){
+    header("location: users.php");
+  }
+?>
+
+<?php include_once "header.php"; ?>
+<body>
+  <div class="wrapper">
+    <section class="form login">
+      <header>Chat Apps</header>
+      <form action="#" method="POST" enctype="multipart/form-data" autocomplete="off">
+        <div class="error-text"></div>
+        <div class="field input">
+          <label>Email Address</label>
+          <input type="text" name="email">
+        </div>
+        <div class="field input">
+          <label>Password</label>
+          <input type="password" name="password">
+        </div>
+        <div class="field button">
+          <input type="submit" name="submit" value="Click to Chat">
+        </div>
+      </form>
+      <div class="link">Not yet signed up? <a href="index.php">Signup</a></div>
+    </section>
+  </div>
+  
+  <script src="javascript/login.js"></script>
+
+</body>
+</html>
